0abb078c91a5d466c0980b7b525c308ed8e0c08d
Wayne Huang <wayneh001@yahoo.com.tw>
2022-02-09 08:23:49 +0800

    Update
    基本排序功能

--- javascript.js
+++ javascript.js
@@ -8,6 +8,7 @@ let data = [];
 let tempData = [];
 let noResult = false;
 let currentCategory = '';
+let criteria = '';
 let currentPage = 1
 let totalPage = 0
 
@@ -75,7 +76,7 @@ function filtering() {
             return item['種類代碼'] === 'N06';
         });
     }
-    renderData(tempData);
+    sorting(criteria);
 }
 
 // 搜尋
@@ -100,15 +101,34 @@ function search() {
     }
 }
 
+// 排序選擇監聽
+select.addEventListener("change", function (e) {
+    criteria = e.target.value;
+    // console.log(criteria);
+    sorting(criteria);
+});
+
+// 進階排序選擇
+
+
 // 排序
-select.addEventListener("click", function (e) {
-
-})
-
-// 進階排序
-mobileSelect.addEventListener("click", function (e) {
-
-})
+function sorting(value) {
+    let sortData = [];
+    if (tempData.length === 0) {
+        alert('請先選擇農產品種類')
+    } else {
+        tempData.forEach(function (item) {
+            parseInt(item[value], 10);
+        })
+        // console.log(typeof tempData[0][value]);
+        sortData = tempData.sort(function(a, b) {
+            // console.log(a[value])
+            return a[value] - b[value];
+        });
+        // console.log(sortData);
+        renderData(sortData);
+    }
+}
 
 // 初始化
 window.onload = function () {
